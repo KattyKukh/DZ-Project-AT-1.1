d15206b2a3ee6c664f5fe40d59919ba478d638ae
Кухоцкая Екатерина <ekukhotskaya@mail.ru>
2022-08-01 15:01:42 +0300

    Testing with Junit4 and Junit5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,5 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
@@ -10,20 +8,34 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int amount = 1_000;
         int actual = service.remain(amount);
-        assertEquals(actual,0);
+        assertEquals(0,actual);
     }
     @org.junit.Test
     public void shouldOfferToPay200IfAmountIs1800() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1800;
         int actual = service.remain(amount);
-        assertEquals(actual,200);
+        assertEquals(200,actual);
     }
     @org.junit.Test
     public void shouldOfferToPay700IfAmountIs300() {
         CashbackHackService service = new CashbackHackService();
         int amount = 300;
         int actual = service.remain(amount);
-        assertEquals(actual,700);
+        assertEquals(700,actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void shouldOfferToPay400IfAmountIs1600() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1600;
+        int actual = service.remain(amount);
+        assertEquals(400,actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void shouldOfferToPay150IfAmountIs850() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 850;
+        int actual = service.remain(amount);
+        assertEquals(150,actual);
     }
 }
